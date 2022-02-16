9a11f3353ec34c56796730309c91c45fed40d62b
derivada <derivadas@protonmail.com>
2022-02-16 11:22:26 +0100

    setup prac1

---
+++ README.md
@@ -0,0 +1,10 @@
+## Arquitectura de Computadores
+
+### Repositorio para las prácticas de Arquitectura de Computadores
+
+##### Doble Grado en Ingeniería Informática y Matemáticas @USC 2021-2022
+
+Grupo de prácticas:
+- Pablo Landrove (@PabloLandro)
+- Pablo Díaz (@derivada)
+
---
+++ prac1/main.c
@@ -0,0 +1,15 @@
+/**
+ * @author Pablo Landrove (@PabloLandro)
+ * @author Pablo Díaz (@derivada)
+ * @brief Práctica 1 Arquitectura de Computadores
+ * @date 2022-02-16
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return EXIT_SUCCESS;
+}
