b899705da460359b443ebf6c4f057ac75623bf8a
PabloLandro <landrovepablo@gmail.com>
2022-03-18 12:38:35 +0100

    Dos gráficas

--- prac1/plots.m
+++ prac1/plots.m
@@ -1,12 +1,15 @@
-1  #!/usr/bin/env octave -q
 ds = importdata ("results.csv", delimiterIn=",", headerLinesIn=1);
 
-colors = ["r", "b", "g", "m", "k"];
+colors = ["r", "b", "g", "m", "k", "c"];
 
+num_L=6;	%Numero de valores distintos de L
+num_D=5;	%numero de valores distintos de D
+
+%Gráfica de Ds
 l = [];
 array_plots = [];
 
-for i = 1:5
+for i = 1:num_D
     indexes = ((i-1)*7)+1:i*7;
     array_plots(i) = plot(ds.data(indexes, 3), ds.data(indexes, 5), "color", colors(i));
     l = [l ds.data(indexes(1), 1)];
@@ -19,3 +22,22 @@ legend(array_plots, cellstr(num2str(l', 'D=%-d')));
 xlabel("L");
 ylabel("ck medio");
 print -djpg graphD-L.jpg;
+
+
+%Para la gráfica de Ls
+l = [];
+array_plots = [];
+
+for i = 1:num_L
+    indexes = (((i-1)*num_D) + 1: i*num_D);
+    array_plots(i) = plot(ds.data(indexes, 1), ds.data(indexes, 5), "color", colors(i));
+    l = [l ds.data(indexes(1), 3)];
+    if (i == 1); hold on; endif;
+endfor
+
+title("Resultados de las ejecuciones");
+
+legend(array_plots, cellstr(num2str(l', 'L=%-d')));
+xlabel("D");
+ylabel("ck medio");
+print -djpg graphL-D.jpg;
