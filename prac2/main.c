c0263285d87be4e637c82578cd7af7f2dc76b6fd
derivada <derivadas@protonmail.com>
2022-04-06 12:45:17 +0200

    primer metodo Co-authored-by: PabloLandro <PabloLandro@users.noreply.github.com>

--- prac2/main.c
+++ prac2/main.c
@@ -11,70 +11,82 @@ void shuffle(int A[], int n);
 static unsigned cyc_hi = 0;
 static unsigned cyc_lo = 0;
 
-typedef struct {
+typedef struct
+{
     double **a, **b, *c, **d, *e, f;
     int *ind;
 } datos;
 
-typedef struct {
+typedef struct
+{
     int ck;
     double ck_medios;
 } tiempos;
+
+// Funciones de inicializacion y liberación de memoria
+void inicializacion(datos *in);
+void freeMyNiggaJerome(datos in);
+
+// Funciones de algoritmos
+tiempos algoritmoSecuencial(datos in);
+tiempos algoritmoSecuencialOptimizado(datos in);
 
 // Variables del experimento
 int N, ALG;
 void leerParametros(int argc, const char *argv[]);
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char *argv[])
+{
     srand(time(NULL));
 
     leerParametros(argc, argv);
-    datos casoPrueba;
-    int accesos = 0;
-    double ck, ck_medio;
+    datos *casoPrueba = (datos *)malloc(sizeof(datos));
+    tiempos results;
 
     inicializacion(casoPrueba);
 
     // Ordenamos el vector ind de forma aleatoria
-    shuffle(casoPrueba.ind, N);
+    shuffle(casoPrueba->ind, N);
 
-
-    switch (ALG) {
+    switch (ALG)
+    {
     case 1:
+        printf("Ejecutando algoritmo secuencial!\n");
+        results = algoritmoSecuencial(*casoPrueba);
+        break;
     case 2:
+        printf("Ejecutando algoritmo secuencial optimizado!\n");
+        results = algoritmoSecuencialOptimizado(*casoPrueba);
+        break;
     case 3:
     case 4:
     default:
         printf("Algoritmo no encontrado!");
         exit(EXIT_FAILURE);
     }
-    /*
-    // Inicializamos el contador
-    start_counter();
-    // Obtenemos los resultados temporales
-    ck = get_counter();
-    ck_medio = (double)(ck / accesos);
-    */
 
     // Imprimimos los resultados
-    printf("f = %4lf\tck = %4lf\tck_medio = %4lf\n", f, ck, ck_medio);
+    printf("Tiempos: ck = %d\tck_medio = %4lf\n", results.ck, results.ck_medios);
 
     // Liberación de mi negro jerónimo
-    freeMyNiggaJerome(casoPrueba);
+    freeMyNiggaJerome(*casoPrueba);
     exit(EXIT_SUCCESS);
 }
 
-void leerParametros(int argc, const char *argv[]) {
-    if (argc <= 3) {
+void leerParametros(int argc, const char *argv[])
+{
+    if (argc != 3)
+    {
         printf("Uso correcto: ./main N ALG\n");
         exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
         // N: tamaño de la operación
         N = atoi(argv[1]);
         // ALG: Algoritmo a usar
         ALG = atoi(argv[2]);
-        if (N <= 0 ||
-            ALG <= 0) // Comprobamos que los valores de entrada son válidos
+        if (N <= 0 || ALG <= 0) // Comprobamos que los valores de entrada son válidos
         {
             printf("Alguno de los argumentos numéricos es incorrecto\n");
             exit(EXIT_FAILURE);
@@ -82,8 +94,10 @@ void leerParametros(int argc, const char *argv[]) {
     }
 }
 
-void freeMyNiggaJerome(datos in) {
-    for (int i = 0; i < N; i++) {
+void freeMyNiggaJerome(datos in)
+{
+    for (int i = 0; i < N; i++)
+    {
         free(in.a[i]);
         free(in.d[i]);
         if (i < 8)
@@ -97,40 +111,47 @@ void freeMyNiggaJerome(datos in) {
     free(in.ind);
 }
 
-void inicializacion(datos in) {
-    in.a = (double **)malloc(N * sizeof(double *));
-    in.b = (double **)malloc(8 * sizeof(double *));
-    in.c = (double *)malloc(8 * sizeof(double));
-    in.d = (double **)malloc(N * sizeof(double *));
-    in.e = (double *)malloc(N * sizeof(double));
-    in.ind = (int *)malloc(N * sizeof(int));
-
-    for (int i = 0; i < N; i++) {
-        in.a[i] = (double *)malloc(sizeof(double) * 8);
-        if (i < 8) {
-            in.b[i] = (double *)malloc(sizeof(double) * N);
+void inicializacion(datos *in)
+{
+    in->a = (double **)malloc(N * sizeof(double *));
+    in->b = (double **)malloc(8 * sizeof(double *));
+    in->c = (double *)malloc(8 * sizeof(double));
+    in->d = (double **)malloc(N * sizeof(double *));
+    in->e = (double *)malloc(N * sizeof(double));
+    in->ind = (int *)malloc(N * sizeof(int));
+    tiempos results;
+    for (int i = 0; i < N; i++)
+    {
+        in->a[i] = (double *)malloc(sizeof(double) * 8);
+        if (i < 8)
+        {
+            in->b[i] = (double *)malloc(sizeof(double) * N);
         }
-        in.d[i] = (double *)malloc(sizeof(double) * N);
-        memset(in.d[i], 0, N * sizeof(double));
+        (in->d)[i] = (double *)malloc(sizeof(double) * N);
+        memset(in->d[i], 0, N * sizeof(double));
     }
-    memset(in.e, 0, N * sizeof(double));
-    in.f = 0;
+    printf("d[0][0]: %lf\n", in->d[0][0]);
 
-    for (int i = 0; i < N; i++) {
-        in.ind[i] = i;
-        in.c[i] = (double)rand() / RAND_MAX * 2.0 -
-                  1.0; // Double en el rango (-1.0, 1.0)
-        for (int j = 0; j < 8; j++) {
-            in.a[j][i] = (double)rand() / RAND_MAX * 2.0 -
-                         1.0; // Double en el rango (-1.0, 1.0)
-            in.b[i][j] = (double)rand() / RAND_MAX * 2.0 -
-                         1.0; // Double en el rango (-1.0, 1.0)
+    memset(in->e, 0, N * sizeof(double));
+    in->f = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        in->ind[i] = i;
+        if (i < 8)
+            in->c[i] = (double)rand() / RAND_MAX * 2.0 - 1.0; // Double en el rango (-1.0, 1.0)
+        for (int j = 0; j < 8; j++)
+        {
+            in->a[i][j] = (double)rand() / RAND_MAX * 2.0 - 1.0; // Double en el rango (-1.0, 1.0)
+            in->b[j][i] = (double)rand() / RAND_MAX * 2.0 - 1.0; // Double en el rango (-1.0, 1.0)
         }
     }
 }
 
-void shuffle(int A[], int n) {
-    for (int i = n - 1; i >= 1; i--) {
+void shuffle(int A[], int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
         int j = rand() % (i + 1);
         int temp = A[i];
         A[i] = A[j];
@@ -145,7 +166,8 @@ void shuffle(int A[], int n) {
 
 /* Set *hi and *lo to the high and low order bits of the cycle counter.
 Implementation requires assembly code to use the rdtsc instruction. */
-void access_counter(unsigned *hi, unsigned *lo) {
+void access_counter(unsigned *hi, unsigned *lo)
+{
     asm("rdtsc; movl %%edx,%0; movl %%eax,%1" /* Read cycle counter */
         : "=r"(*hi), "=r"(*lo)                /* and move results to */
         : /* No input */                      /* the two outputs */
@@ -156,7 +178,8 @@ void access_counter(unsigned *hi, unsigned *lo) {
 void start_counter() { access_counter(&cyc_hi, &cyc_lo); }
 
 /* Return the number of cycles since the last call to start_counter. */
-double get_counter() {
+double get_counter()
+{
     unsigned ncyc_hi, ncyc_lo;
     unsigned hi, lo, borrow;
     double result;
@@ -169,22 +192,41 @@ double get_counter() {
     borrow = lo > ncyc_lo;
     hi = ncyc_hi - cyc_hi - borrow;
     result = (double)hi * (1 << 30) * 4 + lo;
-    if (result < 0) {
+    if (result < 0)
+    {
         fprintf(stderr, "Error: counter returns neg value: %.0f\n", result);
     }
     return result;
 }
 
-tiempos algoritmoSecuencial(datos in) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < 8; k++) {
-                in.d[i][j] += 2 * in.[i][k] * (in.b[k][j] - in.c[k]);
-            }
-        }
+tiempos algoritmoSecuencial(datos in)
+{
+    tiempos results;
+    // accesos = (9*8*N*N) + (N*5*2) = N*(72*N + 10)
+    int accesos = N * (72 * N + 10);
+    printf("in[a][a]: %lf\n", in.a[0][0]);
+    // Inicializamos el contador
+    start_counter();
+    for (int i = 0; i < N; i++)                                        // N iteraciones
+        for (int j = 0; j < N; j++)                                    // N iteraciones
+            for (int k = 0; k < 8; k++)                                // 8 iteraciones
+                in.d[i][j] += 2 * in.a[i][k] * (in.b[k][j] - in.c[k]); // 9 accesos
+
+    for (int i = 0; i < N; i++)
+    {                                             // N iteraciones
+        in.e[i] = in.d[in.ind[i]][in.ind[i]] / 2; // 5 accesos
+        in.f += in.e[i];                          // 2 accesos
     }
-    for (int i = 0; i < N; i++) {
-        e[i] = d[ind[i]][ind[i]] / 2;
-        f += e[i];
-    }
+
+    // Obtenemos los resultados temporales
+    results.ck = get_counter();
+    results.ck_medios = ((double)results.ck / accesos);
+    printf("Resultado del algoritmo secuencial: f = %4lf\n", in.f);
+    return results;
+}
+
+tiempos algoritmoSecuencialOptimizado(datos in)
+{
+    tiempos tiempo;
+    return tiempo;
 }
